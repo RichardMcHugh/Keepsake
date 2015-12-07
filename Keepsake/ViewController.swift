be3239b0fdfb9c4c3fe9d474d9281c9b0166ee00
RichardMcHugh <richardmchugh@outlook.com>
2015-12-07 12:48:40 +0000

    added cocoapods

--- Keepsake/ViewController.swift
+++ Keepsake/ViewController.swift
@@ -1,12 +1,10 @@
-//hello again
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //make some changes
+        
     }
 
 
