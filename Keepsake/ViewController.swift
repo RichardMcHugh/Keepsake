b5138f536be80a75572e65f4250e575a43808389
RichardMcHugh <richardmchugh@outlook.com>
2015-12-07 13:11:50 +0000

    Alamofire import

--- Keepsake/ViewController.swift
+++ Keepsake/ViewController.swift
@@ -1,9 +1,26 @@
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Alamofire.request(.GET, "http://kylegoslan.co.uk/name.json").response { request, response, data, error in
+            
+            if let data = data {
+                
+                let json = JSON(data: data)
+                
+                for thing in json.arrayValue {
+                    print(thing["text"].stringValue)
+                }
+                
+            }
+            
+        }
+        
         
     }
 
